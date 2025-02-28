cd243bfe935d071f20f3b54ae89b3a05feb1a07f
EC2 Default User <ec2-user@ip-172-31-82-238.ec2.internal>
2025-02-28 23:09:24 +0000

    Homework 11 scripts

---
+++ Homework-11/introduce.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#part 1
+
+name=Kanat
+age=24
+favorite_language=Java
+hobby=hiking
+dream="travel all over the world"
+
+echo "Hi, my name is $name, and I'm $age years old. Every morning, $name likes to spend time $hobby.
+As a $age-year-old, I believe it's never too late to follow your dreams. My favorite programming language is $favorite_language.
+I code in $favorite_language almost every day!
+One day, I hope my love for $favorite_language will help me achieve my dream of $dream.
+When I'm not busy working toward $dream, you'll find me $hobby to relax.
+That's a little about me, $name — a $age-year-old who loves $favorite_language, enjoys $hobby, and dreams of $dream!"
+
